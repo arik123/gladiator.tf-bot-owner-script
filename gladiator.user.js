2586033829432a37c5df47d8dc34bf32cf742d89
Maximilián Maliar <12786043+arik123@users.noreply.github.com>
2025-05-29 11:13:04 +0200

    Update gladiator.user.js

--- gladiator.user.js
+++ gladiator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gladiator.tf bot owner script
 // @namespace    https://steamcommunity.com/profiles/76561198320810968
-// @version      1.27
+// @version      10.0
 // @description  A script for owners of bots on gladiator.tf
 // @author       GladiatorTF
 // @grant        GM.getValue
